68eaa85ff3c345953e5023aa1502052daa5c2014
sdics <jhndas@naver.com>
2022-11-30 09:46:35 +0900

    파일 생성

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_kakao2/screens/chat_screen.dart';
+import 'package:flutter_kakao2/screens/friend_screen.dart';
+import 'package:flutter_kakao2/screens/more_screen.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({Key? key}) : super(key: key);
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [
+          FriendScreen(),
+          ChatScreen(),
+          MoreScreen(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        currentIndex: _selectedIndex,
+        backgroundColor: Colors.grey[100],
+        selectedItemColor: Colors.black,
+        unselectedItemColor: Colors.black54,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.user),
+            label: " ",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.comment),
+            label: " ",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(FontAwesomeIcons.ellipsisH),
+            label: " ",
+          ),
+        ],
+      ),
+    );
+  }
+}
