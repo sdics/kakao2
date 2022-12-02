0e79c995eacaea12622e87733023f98bedd24652
sdics <jhndas@naver.com>
2022-12-02 15:10:09 +0900

    완성

--- lib/components/profile_card.dart
+++ lib/components/profile_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_kakao2/models/user.dart';
+import 'package:flutter_kakao2/screens/porfile_screen.dart';
 
 class ProfileCard extends StatelessWidget {
   const ProfileCard({Key? key, required this.user}) : super(key: key);
@@ -7,25 +8,35 @@ class ProfileCard extends StatelessWidget {
   final User user;
   @override
   Widget build(BuildContext context) {
-    return InkWell(
-      onTap: () {},
-      child: ListTile(
-        leading: CircleAvatar(
-          radius: 20,
-          backgroundImage: NetworkImage(
-            user.backgroundImage,
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => ProfileScreen(user: user),
+            ),
+          );
+        },
+        child: ListTile(
+          leading: CircleAvatar(
+            radius: 20,
+            backgroundImage: NetworkImage(
+              user.backgroundImage,
+            ),
           ),
-        ),
-        title: Text(
-          user.name,
-          style: TextStyle(
-            fontWeight: FontWeight.bold,
-            fontSize: 16,
+          title: Text(
+            user.name,
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16,
+            ),
           ),
-        ),
-        subtitle: Text(
-          user.intro,
-          style: TextStyle(fontSize: 12),
+          subtitle: Text(
+            user.intro,
+            style: TextStyle(fontSize: 12),
+          ),
         ),
       ),
     );
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_kakao2/components/chat_card.dart';
+import 'package:flutter_kakao2/models/chat.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Text("ChatScreen");
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("채팅"),
+      ),
+      body: ListView(
+        children: List.generate(
+          chats.length,
+          (index) => ChatCard(chat: chats[index]),
+        ),
+      ),
+    );
   }
 }
